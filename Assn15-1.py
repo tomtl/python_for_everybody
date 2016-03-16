63d27156d3c6b46281a58e1cdda51027776b588a
Tom Lee <tom_t_lee@yahoo.com>
2016-03-16 16:38:38 -0400

    Add genre to assignment 15-1

--- Assn15-1.py
+++ Assn15-1.py
@@ -1,6 +1,45 @@
 import xml.etree.ElementTree as ET
 import sqlite3
 
+def create_db(cursor) :
+    cursor.execute("DROP TABLE IF EXISTS Artist")
+    cursor.execute("DROP TABLE IF EXISTS Genre")
+    cursor.execute("DROP TABLE IF EXISTS Album")
+    cursor.execute("DROP TABLE IF EXISTS Track")
+
+    cursor.execute('''
+        CREATE TABLE Artist (
+            id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            name    TEXT UNIQUE
+        )'''
+    )
+
+    cursor.execute('''
+        CREATE TABLE Genre (
+            id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            name    TEXT UNIQUE
+        )'''
+    )
+
+    cursor.execute('''
+        CREATE TABLE Album (
+            id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            artist_id  INTEGER,
+            title   TEXT UNIQUE
+        )'''
+    )
+
+    cursor.execute('''
+        CREATE TABLE Track (
+            id  INTEGER NOT NULL PRIMARY KEY
+                AUTOINCREMENT UNIQUE,
+            title TEXT  UNIQUE,
+            album_id  INTEGER,
+            genre_id  INTEGER,
+            len INTEGER, rating INTEGER, count INTEGER
+        )'''
+    )
+
 def lookup(dictionary, key):
     found = False
     for child in dictionary:
@@ -16,6 +55,7 @@ track_data = input_data.findall("dict/dict/dict")
 
 connection = sqlite3.connect("tracks.sqlite")
 cursor = connection.cursor()
+create_db(cursor)
 
 for track in track_data :
     if (lookup(track, "Track ID") is None) :
@@ -24,11 +64,12 @@ for track in track_data :
     name = lookup(track, 'Name')
     artist = lookup(track, 'Artist')
     album = lookup(track, 'Album')
+    genre = lookup(track, 'Genre')
     count = lookup(track, 'Play Count')
     rating = lookup(track, 'Rating')
     length = lookup(track, 'Total Time')
 
-    if name is None or artist is None or album is None :
+    if name is None or artist is None or album is None or genre is None:
         continue
 
     print name, album, artist
@@ -46,9 +87,15 @@ for track in track_data :
     cursor.execute("SELECT id FROM Album WHERE title = ?", (album, ))
     album_id = cursor.fetchone()[0]
 
+    cursor.execute(
+        "INSERT OR IGNORE INTO Genre (name) VALUES ( ? )", (genre, )
+    )
+    cursor.execute("SELECT id FROM Genre WHERE name = ?", (genre, ))
+    genre_id = cursor.fetchone()[0]
+
     cursor.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count)
-        VALUES ( ?, ?, ?, ?, ? )''',
-        ( name, album_id, length, rating, count ) )
+        (title, album_id, len, rating, count, genre_id)
+        VALUES ( ?, ?, ?, ?, ?, ? )''',
+        ( name, album_id, length, rating, count, genre_id ) )
 
     connection.commit()
